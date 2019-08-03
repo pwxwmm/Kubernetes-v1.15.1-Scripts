dedc41389c724fe6714f9c601ad13a8ca3160e70
pwxwmm <45311104+pwxwmm@users.noreply.github.com>
2019-08-03 16:23:28 +0800

    Create k8s-openrc.sh

---
+++ k8s-openrc.sh
@@ -0,0 +1,30 @@
+#Author:weimengmeng
+#Date:August 3, 2019
+#Version:v0.1
+#K8s:V1.15.1
+#Docker:V19.03.1
+#This is Kubernetes Install One-Click Scripts
+#--------------------system Config--------------------#
+#Master Node Server Hostname. example:k8s-node1
+#HOST_MASTER_NAME=
+HOST_MASTER_NAME=k8s-node1
+
+#WORKER1 Node Server Hostname. example:k8s-node2
+#HOST_WORKER1_NAME=
+HOST_WORKER1_NAME=k8s-node2
+
+#WORKER2 Node Server Hostname. example:k8s-node3
+#HOST_WORKER2_NAME=
+HOST_WORKER2_NAME=k8s-node3
+
+#Master Node Server IP. example:x.x.x.x
+#HOST_MASTER_IP=
+HOST_MASTER_IP=192.168.217.131
+
+#Worker1 Node Server IP. example:x.x.x.x
+#HOST_WORKER1_IP=
+HOST_WORKER1_IP=192.168.217.132
+
+#Worker2 Node Server IP. example:x.x.x.x
+#HOST_WORKER2_IP=
+HOST_WORKER2_IP=192.168.217.133
